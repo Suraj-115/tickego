305d94c083481ad7e1b638a91618e77708f4a5a3
Suraj-115 <agarwalsuraj863@gmail.com>
2026-04-07 15:13:24 +0530

    User model creation

---
+++ Backend/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type:String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ['user', 'organizationAdmin', "departmentHead"],
+      default: 'user',
+    },
+
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+    },
+
+    interests: [{type: String}],
+
+    location:{
+      country: String,
+      city: String,
+      latitude: Number,
+      longitude: Number,
+    },
+    isVerified: {
+      type: Boolean, 
+      default: false,
+    },
+    otp: String,
+    otpExpiry: Date,
+  },
+  { timestamps: true}
+);
+
+module.exports = mongoose.model("User",userSchema);
