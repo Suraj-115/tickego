52a4f12fc0d2e1b7b307c2956a3bbf98ab66b900
Suraj-115 <agarwalsuraj863@gmail.com>
2026-04-17 17:12:48 +0530

    Password Reset System using OTP

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,6 +2,53 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const sendEmail = require('../utils/sendEmail');
+
+
+exports.forgotPassword = async(req,res) => {
+  try{
+    const {email} = req.body;
+    const user = await User.findOne({email});
+    if(!user) return res.status(400).json({message: "User not found"});
+
+    const otp = Math.floor(100000 + Math.random()*900000).toString();
+
+    user.otp=otp;
+    user.otpExpires = Date.now() + 5*60*1000;
+    await user.save();
+    await sendEmail(email, "TickeGo OTP",`Your OTP is ${otp}`);
+    res.json({message: "OTP sent to email"});
+  }
+  catch(error){
+    res.status(500).json({ error: error.message});
+  }
+}
+
+
+exports.resetPassword = async (req,res)=>{
+  try{
+    const {email,otp,newPassword} = req.body;
+    const user = await User.findOne({email});
+    console.log("Stored OTP:", user.otp);
+    console.log("Entered OTP:", otp);
+    if(!user || user.otp !== String(otp))
+      return res.status(400).json({message:"Invalid OTP"});
+    if(user.otpExpiry < Date.now())
+     return res.status(400).json({message:"OTP has expired"});
+    const hashedPassword = await bcrypt.hash(newPassword,10);
+    user.password = hashedPassword;
+    user.otp=null;
+    user.otpExpiry=null;
+
+    await user.save();
+    res.json({message: "Password reset successful"});
+  }
+  catch(error){
+    res.status(500).json({error: error.message});
+  }  
+};
+
+
 exports.signup = async (req,res) => {
   try {
     const {name, email, password} = req.body;
